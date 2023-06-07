4294d770859e65cdda82682d77f46a01eeca898b
estefanyrojas <estefanyrojas200419@gmail.com>
2023-06-07 17:34:42 -0500

    creando listas no ordenadas

---
+++ Desktop/php/01_ejercicio/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hola mundo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ipsum adipisci reiciendis neque odio totam. Adipisci quae architecto vel error optio eos, nemo vitae, ducimus quidem voluptates rem, sequi cupiditate.</p>
+    <h2>comandos principales de git</h2>
+    <ul>
+        <li><b>git init:</b>incializar un proyecto en Git.</li>
+        <li><b>git config --global user."email": </b>Identificar y configurar localmente el email de un repositorio local</li>
+        <li><b>git config --global user.name "name"</b>Identificar y configurar globalmente el nombre de usuario en un repositorio local</li>
+    </ul>
+</body>
+</html>
